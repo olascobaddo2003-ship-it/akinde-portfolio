44049227a3a92071d918ddbd299b1a314bd60eae
Precious Akinde <akindeola700@gmail.com>
2026-06-16 12:01:26 +0100

    Fix CSS import warning - use proper Next.js CSS import

--- next-app/pages/_app.js
+++ next-app/pages/_app.js
@@ -1,12 +1,12 @@
 import Head from 'next/head'
 import Script from 'next/script'
+import '../public/styles.css'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="/styles.css" />
       </Head>
       <Component {...pageProps} />
       <Script src="/script.js" strategy="afterInteractive" />
