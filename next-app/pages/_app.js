e15bfb72fbe83be17b3ec4949e0acf56bafc982f
Precious Akinde <akindeola700@gmail.com>
2026-06-16 17:58:32 +0100

    Fix missing globals.css import for Next.js app

--- next-app/pages/_app.js
+++ next-app/pages/_app.js
@@ -1,15 +1,5 @@
-import Head from 'next/head'
-import Script from 'next/script'
-import '../public/styles.css'
+import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Component {...pageProps} />
-      <Script src="/script.js" strategy="afterInteractive" />
-    </>
-  )
+  return <Component {...pageProps} />
 }
